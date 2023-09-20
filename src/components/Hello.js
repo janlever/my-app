3f7a029059095e330eb42724beabe96202a98941
Jan Henrik Levertand <janlever@Jans-MacBook-Pro.local>
2023-09-20 15:50:28 +0300

    learned react

---
+++ src/components/Hello.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+// const Hello = ({ name }) => {
+//   return <div>Hello {name}</div>
+// }
+const Hello = ({ name, age }) => (
+  <div>
+    Hello {name} {age}
+  </div>
+)
+
+export default Hello
